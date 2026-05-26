93ad8885a6073f81dca456e4d567bde7d5ac0840
Timothy Kamba <timothykamba2002@gmail.com>
2026-05-26 14:52:22 -0400

    Update backend to use existing DynamoDB lock table

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-ha-terraform-state-bucket"   # Create this bucket first
-    key            = "aws-ha-autoscaling/terraform.tfstate"
+    bucket         = "aws-ha-terraform-state-bucket"   # ← Change this to your actual bucket
+    key            = "ha-3tier/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock"         # Create this DynamoDB table
     encrypt        = true
+    dynamodb_table = "terraform-state-lock"
   }
 }
