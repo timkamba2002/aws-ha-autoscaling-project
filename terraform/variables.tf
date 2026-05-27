4598d7ef6661d0bc6f6b55b5e4efcacc91f4f471
Timothy Kamba <timothykamba2002@gmail.com>
2026-05-27 08:35:53 -0400

    Remove db_password variable - using SSM instead

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,10 @@
-variable "db_password" {
-  type      = string
-  sensitive = true
+# variable "db_password" {
+#   description = "Password for RDS database"
+#   type        = string
+#   sensitive   = true
+# }
+
+# Add any other variables you might need here
+variable "region" {
+  default = "us-east-1"
 }
