3d4a42fc8e3225002bf9238d216782d7efcaced9
Timothy Kamba <timothykamba2002@gmail.com>
2026-05-29 18:01:22 -0400

    fix: uncomment private route table associations (was causing destroy drift)

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -113,12 +113,12 @@ resource "aws_route" "private_nat_route" {
 }
 
 # Associate private subnets with private route table
-# resource "aws_route_table_association" "private_a" {
-#  subnet_id      = aws_subnet.private_a.id
-#  route_table_id = aws_route_table.private_rt.id
-# }
-#
-# resource "aws_route_table_association" "private_b" {
-#  subnet_id      = aws_subnet.private_b.id
-#  route_table_id = aws_route_table.private_rt.id
-# }
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_rt.id
+}
